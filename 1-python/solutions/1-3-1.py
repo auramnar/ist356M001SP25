050be4e3b6b6ca54a98014f9b83a469135ebdd60
Angela <auramnar@syr.edu>
2025-09-15 09:38:25 -0400

    Update 1-3-1.py

--- 1-python/solutions/1-3-1.py
+++ 1-python/solutions/1-3-1.py
@@ -11,3 +11,23 @@ def average (list_of_numbers):
 nums = [10, 20, 10, 5]
 avg = average(nums)
 print(f"Average of {nums} is {avg}")
+
+
+'''
+def average(list_of_numbers):
+    #do stuff
+    total = sum(list_of_numbers)
+    count = len(list_of_numbers)
+    return total/count
+
+'''
+
+
+'''
+def average2(numlist):
+    count = 0
+    total = 0
+for num in numlist:
+    count = count +1
+    total = total +1
+return total/count
