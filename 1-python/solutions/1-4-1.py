1bff5c5cefa4624b8467040862663bdc5f7c186c
Angela <auramnar@syr.edu>
2025-09-22 06:02:48 -0400

    Update 1-4-1.py

--- 1-python/solutions/1-4-1.py
+++ 1-python/solutions/1-4-1.py
@@ -16,6 +16,31 @@ print(nowstr)
 
 
 
+import datetime
+today = datetime.datetime.now()
+print(today.day) 
+
+----
+        
+from datetime import datetime
+             
+today = datetime.now()
+print(today)
+
+birthday = input("Enter your birthday")
+test = datetime.strptime(birthday, '%m/%d/%Y')
+print(test)
+test_str = test.strftime('%A, %B %d %Y')
+print(test_str)
+---
 
 
+from datetime import datetime, timedelta
 
+
+birthday = input("Enter your birthday")
+
+test = datetime.strptime(birthday, '%m/%d/%Y')
+test = test + timedelta(days=1)
+test_str = test.strftime('%A, %B %d %Y')
+print(test_str)
