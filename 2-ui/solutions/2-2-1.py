909961fa5d6f81ba40bc229a724969d28d86a64a
Angela <auramnar@syr.edu>
2025-09-24 09:02:36 -0400

    Update 2-2-1.py

--- 2-ui/solutions/2-2-1.py
+++ 2-ui/solutions/2-2-1.py
@@ -12,4 +12,20 @@ if btn_clicked:
     perimeter = 2 * (length + width)
     st.write(f"Area: {area}")
     st.write(f"Perimeter: {perimeter}")
-    
+
+
+
+# Notes 
+
+import streamlit as st
+
+st.title("Saying Hello.")
+name = st.text_input("And you are?")
+
+if name:
+    st.write(f"Hello, {name}!")
+else:
+    st.write("Enter a name")
+
+
+If I was to write ... st.write(f'Name is {name}')
