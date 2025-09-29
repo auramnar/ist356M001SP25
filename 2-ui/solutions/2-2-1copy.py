4e7a8b079f17d5942e10dfa62d7aa2160d55668c
Angela <auramnar@syr.edu>
2025-09-29 13:14:58 -0400

    Update 2-2-1copy.py

--- 2-ui/solutions/2-2-1copy.py
+++ 2-ui/solutions/2-2-1copy.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 # Initialize session state variables if they don't exist
+#When the script runs for the very first time, the keys 'length' and 'width' will not exist in st.session_state. 
+#This conditional check ensures they are initialized to 0. 
+# This prevents errors later when inputs try to read their default value from the session state
+# Initialize the session state
 
 if 'length' not in st.session_state: #edit
     st.session_state.length = 0
@@ -9,6 +13,8 @@ if 'width' not in st.session_state: #edit
 
 st.title("Area and Perimeter .") # Title of the app
 # User inputs for length and width
+# set their value directly from st.session_state.length and st.session_state.width. 
+# This is how the app remembers the last entered values, especially after a "Clear" operation that triggers a rerun.
 length = st.number_input("Enter the length", value = st.session_state.length) #edit default from session state
 width = st.number_input("Enter the width", value = st.session_state.width) #edit
 
