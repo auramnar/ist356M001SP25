422e3ef15b3c24d49f6f59fb1d3b9ebd1458815b
Angela <auramnar@syr.edu>
2025-11-10 13:18:53 -0500

    Implement scraping of H2 and H3 headings
    Add functionality to scrape and print H2 and H3 headings with indentation.

--- 5-web/solutions/5-1-2.py
+++ 5-web/solutions/5-1-2.py
@@ -5,6 +5,13 @@ Scrape the Sections H2 and H3 from this page:  https://ist256.com/fall2023/sylla
 
 Print the titles, and detect the tag name so that you indent the H3 tags under the H2 tags.
 
+
+# part1
+
+headings = page.query_selector_all("h2, h3")
+for heading in headings:
+    print(heading.inner_text())
+
 # part2
 tag = heading.evaluate(" el => el.tagName")
 text = heading.inner_text()
