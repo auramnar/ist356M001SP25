8acf128a865d2df7d85d1c16ade2245d91c1dcff
Angela <auramnar@syr.edu>
2025-11-12 07:51:44 -0500

    Refactor Playwright script to extract criteria

--- 5-web/solutions/5-2-1.py
+++ 5-web/solutions/5-2-1.py
@@ -34,3 +34,44 @@ def run(playwright: Playwright) -> None:
 
 with sync_playwright() as playwright:
     run(playwright)
+
+
+
+
+'''
+part 2
+
+'''
+
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://ist256.com/fall2023/syllabus")
+    
+    start_element = page.query_selector('h4#criteria-for-project-grade')
+    #print(start_element.inner_text())
+    next_element = start_element.query_selector("~ *")
+    bullet_elements = next_element.query_selector_all("li")
+    
+    criteria = []
+    for bullet_element in bullet_elements:
+        criteria.append(bullet_element.inner_text())
+    
+
+
+    
+    # ---------------------
+    context.close()
+    browser.close()
+
+    return criteria
+
+with sync_playwright() as playwright:
+    criteria = run(playwright)
+    print(criteria)
